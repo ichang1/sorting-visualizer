8e769db2e7c54538104c40b6941057ed798e3014
ichang1 <isaac.chang717@gmail.com>
2021-01-30 16:25:54 -0500

    miscounted comps for quicksort

--- sorting-visualizer/src/Sorting/quickSort.js
+++ sorting-visualizer/src/Sorting/quickSort.js
@@ -4,6 +4,7 @@ const getQuickSortAnimations = (arr) => {
   if (arr.length <= 1) {
     return { animations, statistics };
   }
+  statistics[0].comparisons += 1;
   // const newArr = arr.slice().sort((a, b) => a - b);
   quickSortHelp(arr, 0, arr.length - 1, animations, statistics);
   // for (let i = 0; i < arr.length; i++) {
@@ -19,8 +20,13 @@ const getQuickSortAnimations = (arr) => {
 };
 
 const quickSortHelp = (arr, left, right, animations, statistics) => {
-  if (left >= right) return;
+  if (left >= right) {
+    statistics[statistics.length - 1].comparisons += 1;
+    return;
+  }
+  statistics[statistics.length - 1].comparisons += 1;
   let pivot = partition(arr, left, right, animations, statistics);
+  statistics[statistics.length - 1].comparisons += 1;
   quickSortHelp(arr, left, pivot - (pivot === right), animations, statistics);
   quickSortHelp(arr, pivot + 1, right, animations, statistics);
 };
@@ -41,8 +47,8 @@ const partition = (arr, left, right, animations, statistics) => {
     animations.push({ type: "compare", action: "end", bars: [i, j] });
 
     const { comparisons, accesses } = statistics[statistics.length - 1];
-    statistics.push({ comparisons: comparisons + 2, accesses: accesses + 2 });
-    statistics.push({ comparisons: comparisons + 2, accesses: accesses + 2 });
+    statistics.push({ comparisons: comparisons + 2, accesses: accesses });
+    statistics.push({ comparisons: comparisons + 2, accesses: accesses });
 
     if (arr[i] > piv && arr[j] < piv) {
       animations.push({
@@ -53,27 +59,31 @@ const partition = (arr, left, right, animations, statistics) => {
       [arr[i], arr[j]] = [arr[j], arr[i]];
 
       const { comparisons, accesses } = statistics[statistics.length - 1];
-      statistics.push({ comparisons: comparisons, accesses: accesses + 2 });
+      statistics.push({ comparisons: comparisons + 2, accesses: accesses + 4 });
 
       i += 1;
       j -= 1;
     } else if (arr[i] <= piv) {
-      statistics[statistics.length - 1].comparison += 1;
-      statistics[statistics.length - 1].accesses += 1;
+      statistics[statistics.length - 1].comparison += 3;
+      statistics[statistics.length - 1].accesses += 3;
       i += 1;
     } else {
+      statistics[statistics.length - 1].comparison += 3;
+      statistics[statistics.length - 1].accesses += 3;
       j -= 1;
     }
   }
+  statistics[statistics.length - 1].comparison += 2;
   if (j === mid) {
+    statistics[statistics.length - 1].comparisons += 1;
     let idx = i;
     for (let k = i; k < mid; k++) {
       animations.push({ type: "compare", action: "start", bars: [k, k] });
       animations.push({ type: "compare", action: "end", bars: [k, k] });
 
       const { comparisons, accesses } = statistics[statistics.length - 1];
-      statistics.push({ comparisons: comparisons + 1, accesses: accesses + 1 });
-      statistics.push({ comparisons: comparisons + 1, accesses: accesses + 1 });
+      statistics.push({ comparisons: comparisons + 1, accesses: accesses });
+      statistics.push({ comparisons: comparisons + 1, accesses: accesses });
 
       if (arr[k] <= piv) {
         animations.push({
@@ -84,7 +94,10 @@ const partition = (arr, left, right, animations, statistics) => {
         [arr[k], arr[idx]] = [arr[idx], arr[k]];
 
         const { comparisons, accesses } = statistics[statistics.length - 1];
-        statistics.push({ comparisons: comparisons, accesses: accesses + 2 });
+        statistics.push({
+          comparisons: comparisons + 1,
+          accesses: accesses + 3,
+        });
 
         idx += 1;
       }
@@ -109,13 +122,14 @@ const partition = (arr, left, right, animations, statistics) => {
     animations.push({ type: "pivot", action: "start", idx: pivIdx });
   } else {
     let idx = j;
+    statistics[statistics.length - 1].comparisons += 1;
     for (let k = j; k > mid; k--) {
       animations.push({ type: "compare", action: "start", bars: [k, k] });
       animations.push({ type: "compare", action: "end", bars: [k, k] });
 
       const { comparisons, accesses } = statistics[statistics.length - 1];
-      statistics.push({ comparisons: comparisons + 1, accesses: accesses + 1 });
-      statistics.push({ comparisons: comparisons + 1, accesses: accesses + 1 });
+      statistics.push({ comparisons: comparisons + 1, accesses: accesses });
+      statistics.push({ comparisons: comparisons + 1, accesses: accesses });
 
       if (arr[k] >= piv) {
         animations.push({
@@ -126,7 +140,10 @@ const partition = (arr, left, right, animations, statistics) => {
         [arr[k], arr[idx]] = [arr[idx], arr[k]];
 
         const { comparisons, accesses } = statistics[statistics.length - 1];
-        statistics.push({ comparisons: comparisons, accesses: accesses + 2 });
+        statistics.push({
+          comparisons: comparisons + 1,
+          accesses: accesses + 3,
+        });
 
         idx -= 1;
       }
@@ -157,7 +174,7 @@ const partition = (arr, left, right, animations, statistics) => {
 
   statistics.push({
     comparisons: statistics[statistics.length - 1].comparisons,
-    accesses: statistics[statistics.length - 1].accesses + 2,
+    accesses: statistics[statistics.length - 1].accesses,
   });
 
   return pivIdx;
